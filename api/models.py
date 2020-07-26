dc7a20328b0578d65b716b26641e5e186fb53418
pranaykatariya <pranaykatariya1@gmail.com>
2020-07-26 18:40:39 +0530

    First Commit

---
+++ api/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class Task(models.Model):
+  from_user = models.CharField(max_length=200,null=True, blank=True)
+  to_user = models.CharField(max_length=200, null=True, blank=True)
+  title = models.CharField(max_length=200)
+  completed = models.BooleanField(default=False, blank=True, null=True)
+  bully_rate = models.FloatField()
+  platform = models.CharField(max_length=200,null=True, blank=True)
+  time_now = models.DateTimeField(auto_now_add=True)
+ 
+      
+  def __str__(self):
+    return self.title
