94793a54f26f0461a0f1d6059fda1fd1fce46094
pranaykatariya <pranaykatariya1@gmail.com>
2020-07-28 18:58:33 +0530

    admin site page updated

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
 # Register your models here.
 
 from .models import Task
 
-admin.site.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('from_user', 'to_user', 'title', 'bully_rate', 'platform', 'time_now') 
+    list_filter = ('time_now','platform',)
+    search_fields = ('title', 'from_user', 'to_user')
+
+
+admin.site.register(Task, TaskAdmin)
+admin.site.unregister(Group)
