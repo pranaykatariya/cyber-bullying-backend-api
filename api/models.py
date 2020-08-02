a5142783505c352f5c0027123b0f6076417e3725
pranaykatariya <pranaykatariya1@gmail.com>
2020-08-02 23:31:20 +0530

    Model updated IMage

--- api/models.py
+++ api/models.py
@@ -44,10 +44,10 @@ class Report(models.Model):
 
 class Image(models.Model):
   complain_id = models.AutoField(primary_key=True)
-  platform_id = models.CharField(max_length=64)
+  platform_id = models.CharField(max_length=64, null=True, blank=True)
   url = models.CharField(max_length=128)
-  platform = models.CharField(max_length=64)
-  result = models.CharField(max_length=64)
+  platform = models.CharField(max_length=64, null=True, blank=True)
+  result = models.CharField(max_length=64, null=True, blank=True)
   time_now = models.DateTimeField(auto_now_add=True)
 
 
